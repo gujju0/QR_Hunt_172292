7fa68b42498a6da46fa202de84308960b9bc510e
gujju0 <20ce03vatsal@gmail.com>
2023-10-22 14:16:25 +0530

    init

---
+++ js/questions.js
@@ -0,0 +1,81 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "What is (void*)?",
+    answer: "Kachori",
+    options: [
+      "AV004-B",
+      "AV011-A",
+      "AV104-B",
+      "AV111-C"
+    ]
+  },
+  {
+    numb: 2,
+    question: "What does the mechanism followed by stack",
+    answer: "Khaman",
+    options: [
+      "AV010-A",
+      "AV103-A",
+      "AV012-B",
+      "AV105-C"
+    ]
+  },
+  {
+    numb: 3,
+    question: "In C, if you pass an array as an argument to a function, what actually gets passed?",
+    answer: "Bhakhri",
+    options: [
+      "AV009-A",
+      "AV102-A",
+      "AV013-B",
+      "AV106-C"
+    ]
+  },
+  {
+    numb: 4,
+    question: "In which decade was the American Institute of Electrical Engineers (AIEE) founded?",
+    answer: "Gulab jamun",
+    options: [
+      "AV008-C",
+      "AV101-B",
+      "AV014-C",
+      "AV107-A"
+    ]
+  },
+  {
+    numb: 5,
+    question: "What is part of a database that holds only one type of information?",
+    answer: "Puri",
+    options: [
+      "AV003-B",
+      "AV110-C",
+      "AV005-D",
+      "AV112-B"
+    ]
+  },
+
+  {
+    numb: 6,
+    question: "'OS' computer abbreviation usually means ?",
+    answer: "Facebook",
+    options: [
+      "AV002-D",
+      "AV109-C",
+      "AV006-D",
+      "AV113-C"
+    ]
+  },
+  {
+    numb: 7,
+    question: "In which decade with the first transatlantic radio broadcast occur?",
+    answer: "Google",
+    options: [
+      "AV001-C",
+      "AV108-B",
+      "AV007-D",
+      "AV114-A"
+    ]
+  },
+];
